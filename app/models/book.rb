a93b9240d44cbef8182c830552fdf5672c0a7b37
Matthew Sullivan <mgsullivan.musc@gmail.com>
2015-03-11 21:39:59 -0400

    added AWS storage to paperclip

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,15 @@ class Book < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image
 	has_attached_file :resource
+
+	validates_attachent_content__type :image,
+	content_type: /^image\/(png|gif|jpg)/,
+	message: "Only images allowed for image upload"
+
+	validates_attachment_content_type :resource
+	content_type: ['application/pdf'],
+	message: "Only PDFs allowed for resource upload"
+
+	validates :image, attachment_presence: true, message: "All books must have an image"
+	validates :resource, attachment_presence: true, message: "All books must have a resource"
 end
